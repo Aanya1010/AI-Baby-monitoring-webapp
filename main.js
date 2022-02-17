1a976ba2bba7dc3f9f096a30431e6803bec5f6f5
Aanya1010 <94533796+Aanya1010@users.noreply.github.com>
2022-02-17 16:22:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+alert_sound = "alert_sound.mp4";
+
+
+
+function preload(){
+   
+video=loadVideo(alert_sound.mp4);
+}
+ function draw() {
+for_loop(objects[i].label =  person) {
+if (detected = true) {
+console.log(baby_detected);
+alert_sound.stop()
+
+else if(detected = false) {
+console.log(baby_not_detected);
+alert_sound.play()
+}
+
+else if(array.length > 0) 
+{
+
+    console.log(baby_not_detected);
+    alert_sound.mp4(start)
+    alert_sound.play()
+} 
+}
+
+ }
+
+}
+
+
+
+
+
+fill("#FF0000");
+text("person", 150, 100);
+noFill();
+stroke("#FF0000");
+rect(30, 60, 450, 350);
